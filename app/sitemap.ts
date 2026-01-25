c2c605d76076d4f758ef33f10376e64d46d8fd22
Martin Zadražil <zadram1@gmail.com>
2026-01-25 09:54:26 +0100

    feat: sitemap added

---
+++ app/sitemap.ts
@@ -0,0 +1,22 @@
+import { MetadataRoute } from 'next';
+
+const sitemap = (): MetadataRoute.Sitemap => {
+  const baseUrl = 'https://martinzadrazil.cz';
+
+  return [
+    {
+      url: `${baseUrl}/cs`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/en`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+  ];
+};
+
+export default sitemap;
