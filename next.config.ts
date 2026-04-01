0dd3aba60a898d19c12979ccbb10f6ea30dabb8b
Martin Zadražil <zadram1@gmail.com>
2026-04-01 11:24:43 +0200

    feat: add rewrite for /wolf3d to webstain3d project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/wolf3d',
+        destination: 'https://webstain3d.vercel.app/wolf3d/',
+      },
+      {
+        source: '/wolf3d/:path*',
+        destination: 'https://webstain3d.vercel.app/wolf3d/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
